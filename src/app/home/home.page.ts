c0bcdb7098ef34a8ecd3f1faec713da24e4b4424
Guillaume Rachet <36331252+grachet@users.noreply.github.com>
2021-08-08 10:55:38 +0200

    clean old messages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { AuthenticationService } from './../services/authentication.service';
 
 @Component({
@@ -8,18 +8,52 @@ import { AuthenticationService } from './../services/authentication.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  // votes = [
+  //   {
+  //     "title": "Is eating a good idea ?",
+  //     "answers": {
+  //       "guillaume.rachet@gmail.com": true,
+  //       "gilles": false,
+  //     },
+  //     "hashtag": "#food #health"
+  //   },
+  //   {
+  //     "title": "Blue is the best color ?",
+  //     "answers": {
+  //       "guillaume.rachet@gmail.com": true,
+  //       "gilles": false,
+  //     },
+  //     "hashtag": "#color"
+  //   }
+  // ]
+
+  votes = JSON.stringify({
+    // 1: {
+    //   "title": "Is eating a good idea ?",
+    //   "answers": {
+    //     "guillaume.rachet@gmail.com": true,
+    //     "gilles": false,
+    //   },
+    //   "hashtag": "#food #health"
+    // },
+    // 2: {
+    //   "title": "Blue is the best color ?",
+    //   "answers": {
+    //     "guillaume.rachet@gmail.com": true,
+    //     "gilles": false,
+    //   },
+    //   "hashtag": "#color"
+    // }
+  })
+
+
   constructor(
     public authService: AuthenticationService,
-    private data: DataService) { }
+    // public loadingCtrl: LoadingController,
+    private firestore: AngularFirestore
+  ) { }
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
-  }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
-  }
 
 }
